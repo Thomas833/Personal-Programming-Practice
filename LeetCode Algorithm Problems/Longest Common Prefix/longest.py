24f0920841421dec59d3cf1c4475999a1bfa3be6
Thomas833 <tpgraham@mines.edu>
2026-03-31 22:29:47 -0500

    added files

---
+++ LeetCode Algorithm Problems/Longest Common Prefix/longest.py
@@ -0,0 +1,25 @@
+
+def longestCommonPrefix(strs):
+    if (len(strs) == 1):
+        return strs[0]
+    if len(strs) == 0:
+        return ""
+    index = 1
+    lcp = ""
+    strs.sort()
+    while index <= len(strs[0]):
+        temp = strs[0][:index]
+        if temp != strs[len(strs)-1][:index]:
+            return lcp
+        else:
+            lcp = temp
+        print("index: " + str(index) + " | lcp: " + str(lcp))
+        index +=1
+    return lcp
+    
+
+def main():
+    strs = ["dog","racecar","car"]
+    print(longestCommonPrefix(strs))
+
+main()
